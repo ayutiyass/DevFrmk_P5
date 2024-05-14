fc9a8c5afd93c16d1f2c40ac131976e875c231d1
tiyasayu <ayutiyass66@gmail.com>
2024-05-14 15:17:52 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\RakBukuController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    // return 'Hello World';
+    return view('hello');
+});
+
+Route::get('/biodata', function () {
+    return view('biodata');
+});
+Route::resource('rak_buku', RakBukuController::class);
